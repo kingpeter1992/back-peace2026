cbae6a1160eda9926bc5e2e08e9d020b53d0bd1e
Jean-Pierre KAPETA <jean-pierre.kapeta@tractafric.com>
2026-02-28 18:38:46 +0100

    correction url spring

--- src/main/java/com/king/peace/security/CorsConfig.java
+++ src/main/java/com/king/peace/security/CorsConfig.java
@@ -16,7 +16,8 @@ public class CorsConfig  {
 	  @Bean
 	   public CorsConfigurationSource corsConfigurationSource() {
 	       CorsConfiguration configuration = new CorsConfiguration();
-	       configuration.setAllowedOrigins(List.of("http://localhost:4200","https://peace-app2026.vercel.app/")); // Remplacez par l'origine de votre application Angular
+	       configuration.setAllowedOrigins(List.of("http://localhost:4200",
+		   "https://peace-app2026.vercel.app")); // Remplacez par l'origine de votre application Angular
 	       configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 	       configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 	        configuration.setExposedHeaders(List.of("Content-Disposition")); // ✅ Exposer Content-Disposition
