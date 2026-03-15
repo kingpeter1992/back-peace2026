18aa6f145396627e0385d31d5963f2dea603f536
Jean-Pierre KAPETA <jean-pierre.kapeta@tractafric.com>
2026-03-15 22:39:04 +0100

    dernier correction build

--- src/main/java/com/king/peace/PeaceApplication.java
+++ src/main/java/com/king/peace/PeaceApplication.java
@@ -11,9 +11,7 @@ import java.util.Set;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
